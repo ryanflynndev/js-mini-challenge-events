8a271d6469318a149dfd2b13b47063e88aa2f473
Ryan Flynn <ryanflynn44@gmail.com>
2020-08-26 07:39:52 -0400

    Forgot to Push This

--- index.js
+++ index.js
@@ -39,8 +39,47 @@ function toggleColor(element) {
     element.style.color = "red"
   }
 }
-
+const header = document.querySelector("h1#header");
+console.log(header);
+header.addEventListener('click', function(e) {
+  toggleColor(document.querySelector("h1#header"))
+});
 
 /***** Deliverable 2 *****/
 
+//We first need to grab our form DONE
+//Then we need to make a submit event DONE
+//We need to grab the info from our inputs and store it into an object
+//Then we need to create a new player
+//Then we need to put that player into our DOM
+let form = document.querySelector("#new-player-form");
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  let player =   {
+    number: e.target.number.value,
+    name: e.target.name.value,
+    nickname: e.target.nickname.value,
+    photo: e.target.photo.value
+  };
+  renderPlayer(player);
+})
+
+
+
+
 /***** Deliverable 3 *****/
+
+
+playerContainer.addEventListener("click", e => {
+  console.log(e.target);
+  if (e.target.matches("button.like-button")) {
+    let likes = playerContainer.querySelector("p.likes").innerText
+    let likeNum = likes.replace("likes", "");
+    console.log(likeNum);
+    likeNum = parseInt(likeNum, 10);
+    likes = likeNum + 1 + " likes";
+    playerContainer.querySelector("p.likes").innerText = likes
+  }
+});
+
